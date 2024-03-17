774a25ec7b351f44cfe91f722bb451b4a0347567
antonii1357 <antonii.spam@gmail.com>
2024-03-17 17:55:02 -0600

    Added index.js and gitignore

---
+++ index.js
@@ -0,0 +1,96 @@
+import screenshot from 'screenshot-desktop'
+import { crop } from 'easyimage'
+import fs from 'fs'
+import { sleep } from '@nut-tree/nut-js'
+import { exec as execCallback } from 'child_process'
+import { promisify } from 'util'
+
+const exec = promisify(execCallback)
+
+async function takeSS(name) {
+    await screenshot({ filename: 'screenshot.jpg' })
+    console.log('Took screenshot')
+
+    const imgInfo = await crop({
+        x: 690,
+        y: 210,
+        cropHeight: 600,
+        cropWidth: 460,
+        src: 'screenshot.jpg'
+    })
+    console.log('Resized screenshot')
+    const path = imgInfo.path
+    const data = fs.readFileSync(path)
+    fs.writeFileSync(name || 'output.jpg', data)
+}
+
+const scriptPath = 'inference.py'
+
+const fieldArray = [
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ], // 18 spaces of width
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    // This is the middle, the bridge between blue and red areas
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+]
+
+while (true) {
+    console.log('Running:')
+    const img = await takeSS()
+
+    try {
+        const { stdout } = await exec(`py "${scriptPath}"`)
+
+        let cards = [],
+            confidences = [],
+            coords = []
+
+        const labelRegEx = /Label (\w+): (\d+)%/g,
+            coordsRegEx = /Coords: (\d+) (\d+) (\d+) (\d+)/g
+
+        let labelMatch
+        while ((labelMatch = labelRegEx.exec(stdout)) !== null) {
+            cards.push(labelMatch[ 1 ])
+            confidences.push(parseInt(labelMatch[ 2 ]))
+        }
+
+        let coordsMatch
+        while ((coordsMatch = coordsRegEx.exec(stdout)) !== null) {
+            coords.push([ parseInt(coordsMatch[ 1 ]), parseInt(coordsMatch[ 2 ]), parseInt(coordsMatch[ 3 ]), parseInt(coordsMatch[ 4 ]) ])
+        }
+
+        console.log("Cards:", cards)
+        console.log("Confidences:", confidences)
+        console.log("Coordinates:", coords)
+
+
+    } catch (error) {
+        console.error(`exec error: ${error}`)
+    }
+
+    sleep(1000)
+}
